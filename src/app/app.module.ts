5ba60aab9b68f6a79058281db99dfcde6bb90f60
caujasutom <nadruk@seznam.cz>
2023-02-09 18:22:42 +0100

    <3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/template/navbar/navbar.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { LandingComponent } from './components/template/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
