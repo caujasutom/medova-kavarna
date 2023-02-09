7a3b81995a8e369e96aeb30eb252190448a4b052
caujasutom <nadruk@seznam.cz>
2023-02-09 19:00:11 +0100

    <3

--- src/app/components/template/navbar/navbar.component.ts
+++ src/app/components/template/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  showNavbar = false;
 
+  constructor(private router: Router) {}
+
+  toggleNavbar() {
+    this.showNavbar = !this.showNavbar;
+  }  
+
+  scrollToContact() {
+    this.toggleNavbar()
+    this.router.navigate(['/']).then(() => {
+      setTimeout(() => {
+        document.querySelector('#kontakt')!.scrollIntoView({ behavior: 'smooth' });
+      }, 10);
+    });
+  }
 }
