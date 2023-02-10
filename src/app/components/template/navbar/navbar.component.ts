3b1981d9d85c7d6ad92226c28f273c1873e47c5e
caujasutom <nadruk@seznam.cz>
2023-02-10 21:46:24 +0100

    <333

--- src/app/components/template/navbar/navbar.component.ts
+++ src/app/components/template/navbar/navbar.component.ts
@@ -1,14 +1,58 @@
-import { Component, HostListener } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  showNavbar = false;
+  toggleNavbar() {
+    if (window.scrollY == 0 && !this.showNavbar) {
+      this.renderer.addClass(
+        this.el.nativeElement.querySelector('header'),
+        'active'
+      );
+          this.showNavbar = !this.showNavbar;
+      return;
+    } else if (window.scrollY == 0 && this.showNavbar) {
+      this.renderer.removeClass(
+        this.el.nativeElement.querySelector('header'),
+        'active'
+      );
+      this.showNavbar = !this.showNavbar;
+      return;
+    }
+    this.showNavbar = !this.showNavbar;
+  }
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY != 0) {
+        this.renderer.addClass(
+          this.el.nativeElement.querySelector('header'),
+          'active'
+        );
+      } else {
+        if (!this.showNavbar) {
+          this.renderer.removeClass(
+            this.el.nativeElement.querySelector('header'),
+            'active'
+          );
+        }
+      }
+    });
+  }
   scroll(id: string) {
+    this.toggleNavbar();
     const element = document.getElementById(id)!;
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+  test(sectionId: string) {
+    event!.preventDefault();
+    const element = document.getElementById(sectionId)!;
+    window.location.replace(`#${sectionId}`);
     element.scrollIntoView({ behavior: 'smooth' });
   }
 }
